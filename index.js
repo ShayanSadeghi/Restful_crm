3c47350265f469fbf5bc76ad84e66ad2ce365c13
Shayan <Shayansadeghi1996@gmail.com>
2020-05-25 01:06:38 +0430

    added mongoose and body parser

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-import express from 'express';
-import routes from './src/routes/crmRoutes';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import routes from "./src/routes/crmRoutes";
 
 const app = express();
 const PORT = 3000;
 
+//Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost/crm_db")
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 routes(app);
 
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   res.send(`App running on port ${PORT}`);
-})
+});
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`App listen to port ${PORT}`);
-})
+});
