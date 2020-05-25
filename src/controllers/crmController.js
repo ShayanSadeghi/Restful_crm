fe6bf9a78cd92ffc2ff854da7bfce68525ee03a8
Shayan <Shayansadeghi1996@gmail.com>
2020-05-25 21:08:31 +0430

    Read one contact using id

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -1,24 +1,32 @@
-import mongoose from 'mongoose';
-import ContactSchema from '../models/crmModel';
+import mongoose from "mongoose";
+import ContactSchema from "../models/crmModel";
 
-const Contact = mongoose.model('Contact',ContactSchema);
+const Contact = mongoose.model("Contact", ContactSchema);
 
 export const addNewContact = (req, res) => {
-
   const newContact = new Contact(req.body);
 
-  newContact.save((err,contact)=>{
-    if (err){
+  newContact.save((err, contact) => {
+    if (err) {
       return res.status(400).send(err);
     }
     res.json(contact);
   });
 };
 
-export const showAllContacts = (req,res)=>{
+export const showAllContacts = (req, res) => {
   Contact.find()
-  .then(result=>{
-    res.json(result);
-  })
-  .catch(err => console.log(err))
-}
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) =>  res.status(400).send(err));
+};
+
+export const showContactById = (req, res) => {
+  Contact.findById(req.params.contactId).then((contact)=>{
+    if(!contact){
+      return res.status(404).json({contact:"This contact id was not found!"})
+    }
+    res.json(contact);
+  }).catch(err => res.status(400).send(err))
+};
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,10 +1,11 @@
-import { addNewContact,
-   showAllContacts
- } from "../controllers/crmController";
+import {
+  addNewContact,
+  showAllContacts,
+  showContactById,
+} from "../controllers/crmController";
 
 const routes = (app) => {
-  app
-    .route("/contact")
+  app.route("/contact")
     .get(
       (req, res, next) => {
         //middleware
@@ -12,12 +13,13 @@ const routes = (app) => {
         console.log(`request type: ${req.method}`);
         next();
       },
-      (req, res) => showAllContacts(req,res))
+      (req, res) => showAllContacts(req, res)
+    )
 
     .post((req, res) => addNewContact(req, res));
 
-  app
-    .route("/contact/:contactId")
+  app.route("/contact/:contactId")
+    .get((req,res)=>showContactById(req, res))
     .put((req, res) => {
       res.send("PUT works well!");
     })
